05a6f0b127c43d922809d7791a07c5ee508bf34d
jastion <r05921086@ntu.edu.tw>
2017-03-16 17:43:00 +0800

    Read CSV file functionality

--- hw1/lin_grad.py
+++ hw1/lin_grad.py
@@ -1,11 +1,16 @@
 import sys
-import numpy as py
+import numpy as np
 import matplotlib as pl
 
 print (sys.argv[0])  
 print (sys.argv[1])
 print (sys.argv[2])
-csv_training  = np.genfromtxt(sys.argv[1], delimiter=',')
-csv_testing = np.genfromtext(sys.argv[2],delimiter=',')
+
+header_training = np.genfromtxt(sys.argv[1], skip_header=True, delimiter = ",",names=True)
+csv_training  = np.genfromtxt(sys.argv[1], skip_header=True, delimiter = ",",names=True)
+csv_testing = np.genfromtxt(sys.argv[2], skip_header=True, delimiter = ",")
 
 
+print (csv_training)
+print (csv_training.shape)
+print (header_training)
