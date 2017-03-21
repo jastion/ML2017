b6ac15f278e1a51e199d84d06bfab1cddb7a8cd1
jastion <r05921086@ntu.edu.tw>
2017-03-21 19:02:04 +0800

    completed linear gradient descent with unfinished neural network functions

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,34 +1,42 @@
+# Machine Learning 2017
+# Homework 1: 	Linear Regression
+# Instructor: 	李宏毅 (Hung-yi Lee)
+# Student: 		邱名彦 / Michael Chiou
+# Student ID: 	R05921086
+# Email:		r05921086.ntu.edu.tw 
+# Github: 		https://github.com/jastion/ML2017.git
+
 import sys
 import numpy as np
 import matplotlib as mpl
 import time
-import csv
 from lin_grad import LineGradDesc
 #from GradientDescent import GradDesc
 
-# 0  AMB_TEMP		9	PM2.5  
-# 1  CH4			10	RAINFALL
-# 2  CO 			11	RH (Rel. Humidity)
-# 3  NMHC			12	SO2
-# 4  NO 			13 	THC
-# 5  NO2 			14 	WD_HR
-# 6  NOx 			15	WIND_DIRECT
-# 7  O3				16	WIND_SPEED
-# 8  PM10			17 	WS_HR
+# 0  	AMB_TEMP		9	O	PM2.5  
+# 1  	CH4				10	O	RAINFALL
+# 2  	CO 				11	X	RH (Rel. Humidity)
+# 3  	NMHC			12	X	SO2
+# 4  X	NO 				13 		THC
+# 5  	NO2 			14 		WD_HR
+# 6  	NOx 			15	O	WIND_DIRECT
+# 7  	O3				16	O	WIND_SPEED
+# 8  O	PM10			17 		WS_HR
 
 
 np.set_printoptions(linewidth=1e3, edgeitems=1e2, suppress=True,precision=3)
 
+#Read in Training Data and preprocess
 csvTraining  = np.genfromtxt(sys.argv[1], dtype="S", skip_header=True, delimiter = ",")
 csvTraining = csvTraining[:,3:] 
-csvTesting = np.genfromtxt(sys.argv[2], dtype="S", skip_header=False, delimiter = ",")
-csvTesting = csvTesting[:,2:]
-
-
 setTrain = csvTraining[:18,:]
 
 for days in range(1,12*20):
     setTrain = np.append(setTrain, csvTraining[days*18:days*18+18,:],1)
+
+#Read in Testing data and preprocess
+csvTesting = np.genfromtxt(sys.argv[2], dtype="S", skip_header=False, delimiter = ",")
+csvTesting = csvTesting[:,2:]
 
 setTrain[setTrain == "NR"] = 0#(if array have "NR" string, let it convert to float)
 setTrain = setTrain.astype(np.float)#(18,5760)
@@ -41,23 +49,25 @@ for days in range(1,12*20):
 setTest[setTest == "NR"] = 0
 setTest = setTest.astype(np.float)#(18,2160)
 
-features = np.arange(18)
+#Feature selection and time range
+#features = np.array([9])
+#features = np.arange(18)
 #features = np.array([8,9,10,16]) 5.72 5.9, 6.19
 #features = np.array([8,9,10,16]) #5.89221 5,86 6.17
-features = np.array([4,5,6,8,9,10,16])
-#features = np.array([9,10,16])
-features = np.array([9])
+#features = np.array([4,5,6,8,9,10,16])
+#features = np.array([8,9,10,16]) #5.78
+#features = np.array([4,8,9,10,16])
+#features = np.array([9,10,12,16])
+features = np.array([8,9,10,15,16])
 hours = np.arange(9)
 print ("Initializing")
 
+#Run Gradient Descent
 lineGrad = LineGradDesc(setTrain, setTest , features, hours, 0.2) 
-#lineGrad.grad_desc(100000, 0.5)
-#lineGrad.run_test_set()
-lineGrad.neural_network(5000,0.1)
+lineGrad.grad_desc(100000, 1000)
+lineGrad.run_test_set()
+
+#Not implemented
+#lineGrad.random()
+#lineGrad.neural_network(2000,0.1)
 #lineGrad.test_nn()
-#predict = GradDesc(csvTraining, csvTesting, valid_percent = 0.2)
-##predict.train_grad_ada(interation = 200, lr_rate = 0.2)
-#predict.test_function()
-#lineGrad.run_test_set()
-filename = time.strftime("%Y%m%d-%H%M%S_output.csv")
-#np.savetxt(filename, csv_output,fmt="%s", delimiter=","), 
--- hw1/lin_grad.py
+++ hw1/lin_grad.py
@@ -1,112 +1,107 @@
+# Machine Learning 2017
+# Homework 1: 	Linear Regression
+# Instructor: 	李宏毅 (Hung-yi Lee)
+# Student: 		邱名彦 / Michael Chiou
+# Student ID: 	R05921086
+# Email:		r05921086.ntu.edu.tw 
+# Github: 		https://github.com/jastion/ML2017.git
+
 import sys
 import numpy as np
 import matplotlib as mpl
 import time
-import csv
 
-
+#adjust print options to terminal
 np.set_printoptions(linewidth=1e3, edgeitems=1e10)
 
-
-# 0  AMB_TEMP		9	PM2.5  
-# 1  CH4			10	RAINFALL
-# 2  CO 			11	RH (Rel. Humidity)
-# 3  NMHC			12	SO2
-# 4  NO 			13 	THC
-# 5  NO2 			14 	WD_HR
-# 6  NOx 			15	WIND_DIRECT
-# 7  O3				16	WIND_SPEED
-# 8  PM10			17 	WS_HR
-
 class LineGradDesc:
-
+	#This class contains functions to perform Gradient
 	def __init__(self, inputTraining, inputTest, \
 		inputFeatures,inputHours,inputVerify,):
-
+		#Initializes Linear Gradient Descent Class
+		#Preprocesses and sorts necessary data
+		
 		#save input data
-		self.idxFeatures = inputFeatures
-		self.rangeHours = inputHours
-		self.percentVerify = inputVerify
-		self.setTraining = inputTraining
-		self.setTesting = inputTest
+		self.idxFeatures = inputFeatures #Which features to use
+		self.rangeHours = inputHours #range of hours (0-9)
+		self.percentVerify = inputVerify #% of data to store as Validation
+		self.setTraining = inputTraining #Training Data set
+		self.setTesting = inputTest #Testing Set
 
-		#Initialize extra variables
-		self.numWeights = len(self.idxFeatures) * len(self.rangeHours)
-		print("numWeights: %d" %(self.numWeights))
-		self.weights = np.random.rand(self.numWeights,1)
-		#self.bias = np.array([20]).reshape(1,1)
+		#Initialize Number of Features,  Weights, Bias
+		self.numWeights = len(self.idxFeatures) * len(self.rangeHours) 
+		self.weights = np.random.rand(self.numWeights,1) 
 		self.bias = np.random.rand(1,1)
 
-		#Preprocess Data
+		#Preprocess and Sort Training and Testing Data 
 		self.setTraining = self.sort_training_data()
 		self.setTesting = self.sort_testing_data()
 		
-		self.setTraining, self.meanTraining, self.stDevTraining = self.norm_features(self.setTraining)
-		#print (self.setTraining[0,:])
-		#np.savetxt("output2.csv", self.setTraining,fmt="%s", delimiter=","), 
-		
+		#Normalize Training Data
+		self.setTraining, meanI, stDevI= self.norm_features(self.setTraining)
+		self.meanTraining = meanI
+		self.stDevTraining = stDevI
+
+		#Shuffle data
 		np.random.shuffle(self.setTraining)
-		#features*hours*
-		#np.savetxt("output1.csv", self.setTraining,fmt="%s", delimiter=","),
-		#print (self.setTraining.shape)
 		
-		#self.setTesting, self.meanTesting, self.stDevTesting = self.norm_features(self.setTesting)
-		#print(self.setTraining.shape)
+		#Get total number of available data points
 		numData = self.setTraining.shape[0]
+		#Get Index to segment data
 		idxSegment = numData - int(numData*inputVerify)
 		#print (idxSegment)
 
+		#Segments total training data into validation and training data sets
 		self.setValidation = self.setTraining[idxSegment:,:]
 		self.setTraining = self.setTraining[:idxSegment,:]
-
-		#print (self.setTrainingPM25.shape)
-		#print (self.setValidationPM25.shape)
-		#print (self.setTraining.shape)
-		#print (self.setValidation.shape)
 		
 		print ("Initialize Complete!")
 
 	def sort_training_data(self):
-		#read test_data file
-		# This is an array which will be the shape of (5652, 163) and be returned.
+		#Initializes variables to store sorted training data 
+		#PM2.5 truth values will be appended as the last row to setTraining
+		#This is an array which will be the shape of (5652, 163).
 		setX = np.array([]).reshape(0,len(self.idxFeatures)*len(self.rangeHours))
 		pm25 = np.array([]).reshape(0,(24*20-9)*12)
 
+		#Iterates through data into N x Y where N is available 
+		#data points and Y is number of features 
 		for months in range(12):
 		    for hours in range(self.rangeHours[0],24*20-len(self.rangeHours)):
-		        temp = self.setTraining[self.idxFeatures[:,None],hours+months*480:hours+months*480+len(self.rangeHours)].\
+		        temp = self.setTraining[self.idxFeatures[:,None],hours+months\
+		        *480:hours+months*480+len(self.rangeHours)].\
 		        flatten().reshape(1,len(self.idxFeatures)*len(self.rangeHours))
 		        setX = np.vstack((setX,temp))#(5652,162)
 
-		#Append the correct pm25 value to train_x_set
+		#Append the PM25 truth value to train_x_set
 		for months in range(12):
-			pm25 = np.append(pm25, self.setTraining[len(self.rangeHours),len(self.rangeHours)+months*480:480+months*480])
+			pm25 = np.append(pm25, self.setTraining[len(self.rangeHours),\
+				len(self.rangeHours)+months*480:480+months*480])
 		pm25 = pm25.reshape(setX.shape[0],1)#(5652,1)
+
 		setTraining = np.append(setX,pm25,axis = 1)#(5652,163)
-		#np.savetxt("outputSetX.csv", setX,fmt="%s", delimiter=","), 
 		return setTraining
 
 	def sort_testing_data(self):
-		setTest = self.setTesting[self.idxFeatures[:,None],self.rangeHours].flatten().reshape(1,len(self.idxFeatures)*len(self.rangeHours))
+		#Sorts testing data into an array of size N x Y where N is available 
+		#data points and Y is number of features  
+		setTest = self.setTesting[self.idxFeatures[:,None],self.rangeHours]\
+		.flatten().reshape(1,len(self.idxFeatures)*len(self.rangeHours))
 		for days in range(1,12*20):
-			setTest = np.vstack((setTest,self.setTesting[self.idxFeatures[:,None],self.rangeHours+days*9].flatten()))
+			setTest = np.vstack((setTest,self.setTesting[\
+				self.idxFeatures[:,None],self.rangeHours+days*9].flatten()))
 
 		return setTest
 
 
-
 	def cost_fcn(self):
-		'''
-		loss function for linear regression
-		takes form of the following:
-		L(f) = sum (y_n - y) where y is a linear line
-		'''
+		#loss function for linear regression takes form of the following:
+		#L(f) = sum (y_n - y) where y is a linear line
+		
 		setTrain = self.setTraining[:,:-1]
-
 		setValidation = self.setValidation[:,:-1]
 		
 		yTrain= setTrain.dot(self.weights) + self.bias
-
 		yValid = setValidation.dot(self.weights) + self.bias
 
 		lossTrain = self.rmse(self.setTraining[:,-1], yTrain)
@@ -115,6 +110,8 @@ class LineGradDesc:
 		return lossTrain, lossValid
 		
 	def rmse(self, actual, predicted):
+		#calculates root mean square for error tracking of form
+		#sqrt( (sum of errors)^2)
 		sumError = 0.0
 		for i in range(len(actual)):
 			predictionError = predicted[i] - actual[i]
@@ -126,8 +123,7 @@ class LineGradDesc:
 
 	def norm_features(self, setInput):
 		'''
-		Returns a normalized version of X.
-		normalized X is calculated as follows
+		Returns a normalized version of X. Normalized X is calculated as follows
 		X_Norm = Xi - mean_i) / std_i
 		This sets mean to 0
 		'''
@@ -155,72 +151,72 @@ class LineGradDesc:
 		return 0
 
 	def grad_desc(self, iterations, eta):
-		self.eta = eta;
-		self.iterations = iterations
-		#self.errorValid = np.array([]).reshape(iterations,1)
-		#self.errorTrain = np.array([]).reshape(iterations,1)
-		dwTotal = 1
-		dbTotal = 1
-		valid_loss_error = 0
-		train_loss_error = 0
+		self.eta = eta; #learning rate
+		self.iterations = iterations #number of iterations
+
+		#Initialize variables
+		dwTotal = 1 #total weight differential
+		dbTotal = 1 #total bias differential
+		valid_loss_error = 0 #Validation error
+		train_loss_error = 0 #Training error
 
 		np.random.seed(1)
-		X = self.setTraining[:,:-1].reshape(self.setTraining.shape[0],self.setTraining.shape[1]-1)#(4521,162)
+		#Get training data without truth values (last row)
+		X = self.setTraining[:,:-1].reshape(self.setTraining.shape[0],\
+										self.setTraining.shape[1]-1)#(4521,162)
+		#Get truth values for trianing data
 		yActual = self.setTraining[:,-1].reshape(self.setTraining.shape[0],1)#(4521,1)
 		
+		#Iterate through gradient descent
 		for idx in range(1,iterations+1):
-			dw = 0
-			db = 0
+			# Equation: y = b + sum(weights * X)
+			dw = 0 #weight derivative
+			db = 0 #bias derivative
 
-
+			#calculate prediction
 			yPredict = X.dot(self.weights) + self.bias
-
+			#Determine error
 			deltaError = yPredict - yActual
 
+			#calculate error and bias errors
 			tmpWeight = (2 * deltaError *(-X)).T
 			tmpBias = 2 * deltaError * (-1)
 
+			#determine derivatives
 			diffWeight = np.sum(tmpWeight,1).reshape(tmpWeight.shape[0],1)
 			diffBias = np.sum(tmpBias,0).reshape(1,1)
 
+			#sum up total errors
 			dwTotal += diffWeight**2
 			dbTotal += diffBias**2
+			#update weights
 			self.weights += (eta * diffWeight)/np.sqrt(dwTotal)
 			self.bias += (eta * diffBias)/np.sqrt(dbTotal)
-			'''
-			if (diffWeight < 0):
-				self.weights += (eta * diffWeight)/np.sqrt(dwTotal)
-			else:
-				self.weights -= (eta * diffWeight)/np.sqrt(dwTotal)
 
-			if (diffBias < 0):
-				self.bias += (eta * diffBias)/np.sqrt(dbTotal)
-			else:
-				self.bias -= (eta * diffBias)/np.sqrt(dbTotal)
-			'''
+			#print training and validation losses
 			if idx%1000 == 0 or idx == 1:
 				errorValid, errorTrain = self.cost_fcn()
-				print ("Iterations: %d Valid cost: %f Train cost:  %f" %(idx,errorValid,errorTrain))
-				#self.errorValid = errorValid
-				#self.errorTrain = errorTrain
+				print ("Iterations: %d Valid cost: %f Train cost: \
+				 %f" %(idx,errorValid,errorTrain))
 		print("Descent Complete!")
 		return 0
 
 	def run_test_set(self):
-		#print(self.meanTraining.shape)
-		#print(self.stDevTraining.shape)
-		#print(self.setTesting.shape)
+		#Runs test set on trained weights
 		setTesting = ((self.setTesting-self.meanTraining)/self.stDevTraining)
 		prediction = setTesting.dot(self.weights)+self.bias
 
+		#initialize variables to save to CSV files
 		csvOutput = np.zeros((240+1,1+1), dtype ="|S6")
+		#append labels and headers
 		csvOutput[0,0] = "id"
 		csvOutput[0,1] = "value"
 
+		#append data
 		for idx in range (240):
 			csvOutput[idx+1,0] = "id_" + str(idx)
 			csvOutput[idx+1,1] = float(prediction[idx,0])
-
+		#Write data to CSV
 		np.savetxt("./data/w_pm25.csv", self.weights, delimiter = ",", fmt = "%s")
 		np.savetxt("./data/b_pm25.csv", self.bias, delimiter = ",", fmt = "%s")
 		np.savetxt("./data/test_output.csv", csvOutput, delimiter=",", fmt = "%s")
@@ -228,118 +224,100 @@ class LineGradDesc:
 		print("Save Complete!")
 
 	def sigmoid(self,x):
-
-		output = 1.0/(1.0+(np.exp(-x)))
+		#Equation: 1/(1+e^-t)
+		output = 1.0/(1.0+(np.exp(-x/c)))
 		return output
 
 	def sigmoid_output_to_derivative(self,output):
+		#Sigmoid derivative function
 		return output*(1-output)
 
 	def neural_network(self, iterations, eta):
+		#Non-implemented Neural Network function
+		#Developer Notes:
+		#	To be Implemented
+		#		Back Propagation
+		#		Error checking
+		#		Different activation Functions
+		#		Variable Hidden Layers?
+
 		self.eta = eta;
 		self.iterations = iterations
 		#self.errorValid = np.array([]).reshape(iterations,1)
 		#self.errorTrain = np.array([]).reshape(iterations,1)
-		dwTotal = 1
-		dbTotal = 1
-		valid_loss_error = 0
-		train_loss_error = 0
-		#np.linalg.norm(self.setTraining)
+		
+		#Seed
 		np.random.seed(1)
+		#Collect Training and Ground Truth set
 		X = self.setTraining[:,:-1].reshape(self.setTraining.shape[0],self.setTraining.shape[1]-1)#(4521,162)
-		X.astype("longfloat")
-		#print(X.shape)
 		y = self.setTraining[:,-1].reshape(self.setTraining.shape[0],1)#(4521,1)
-		#print("X Shape: " + str(X.shape))
-		#print(X[:2,:])
 
-		xValidation = self.setValidation[:,:-1]
+		#Collect Validation and Ground Truth set
+		xValid = self.setValidation[:,:-1].reshape(self.setValidation.shape[0],self.setValidation.shape[1]-1)#(4521,162)
+		yValid = self.setValidation[:,-1].reshape(self.setValidation.shape[0],1)#(4521,1)
 
-		np.random.seed(1)
-		hiddenLayer = 5
-		syn0 = 2*np.random.random((self.numWeights,int(np.mean(self.numWeights)))) - 1
+		#Hidden Layer nodes
+		hiddenLayer = 100
+		#initialize synapses
+		syn0 = 2*np.random.random((self.numWeights,hiddenLayer)) - 1
 		#syn0 = 2*np.random.random((self.numWeights,1)) - 1
-		syn1 = 2*np.random.random((int(np.mean(self.numWeights)),1)) - 1	
+		syn1 = 2*np.random.random((hiddenLayer,1)) - 1	
 
+		#initialize previous weights
 		prevSynWeightUpdate0 = np.zeros_like(syn0)
 		prevSynWeightUpdate1 = np.zeros_like(syn1)
 
 		syn0DirCount = np.zeros_like(syn0)
 		syn1DirCount = np.zeros_like(syn1)
-
 		#alphas = [0.0001,0.001,0.01,0.1,1,10,100,1000]
 		alphas = [0.0001]
 		for alpha in alphas:
 			#print (alpha)
+			#Iterate over neural network
 			for idx in xrange(iterations):
-				
 				layer0 = X
-				layer0V = 0
-				#np.savetxt("NN.csv", np.dot(layer0,syn0), delimiter = ",", fmt = "%s")
+				layer0V = xValid
 
 				layer1 = self.sigmoid(np.dot(layer0,syn0))
-				#print("layer1 shape:" + str(layer1.shape))
-				#print(layer2.shape)
-				#print(syn1.shape)
 				layer2 = self.sigmoid(np.dot(layer1,syn1))
 
+				layer1V = self.sigmoid(np.dot(layer0V,syn0))
+				layer2V = self.sigmoid(np.dot(layer1V,syn1))
+
+				errorLayer2V = yValid - layer2V
 				errorLayer2 = y - layer2
 
 				deltaLayer2 = errorLayer2*self.sigmoid_output_to_derivative(layer2)
-				#print("delta layer2: " + str(deltaLayer2.shape))
 				errorLayer1 = deltaLayer2.dot(syn1.T)
-				#errorLayer1 = y - layer1
-				if (idx % 1000) == 0:
-					print ("Error:" + str(np.mean(np.abs(errorLayer1))))
 				deltaLayer1 = errorLayer1*self.sigmoid_output_to_derivative(layer1)
 
-				#print("Layer 1 Shape: " + str(layer1.shape))
-				#print("layer 2 shape: " + str(layer2.shape))
-				#print("syn0 shape:" + str(syn0.shape))
-				#print("syn1 shape:" + str(syn1.shape))
+				#errorLayer1 = y - layer1
+				if (idx % 1000) == 0:
+					a = str(np.mean(np.abs(errorLayer2)))
+					b = str(np.mean(np.abs(errorLayer1)))
+					c = str(np.mean(np.abs(errorLayer2V)))
+					print ("Error1:" + b + "   Error2: " + a + "    Validation: " + c)
+
 				synWeightUpdate0 = layer1.T.dot(deltaLayer2)
 				synWeightUpdate1 = layer2.T.dot(deltaLayer1)
-				#print("syn update0: " + str(synWeightUpdate0.shape))
-				#print("syn update1: " + str(synWeightUpdate1.shape))
 
 				#if idx > 0:
 			#		syn0DirCount += np.abs(((synWeightUpdate0 > 0)+0) - ((prevSynWeightUpdate0 > 0) + 0))
 		#			syn1DirCount += np.abs(((synWeightUpdate1 > 0)+0) - ((prevSynWeightUpdate1 > 0) + 0))
 				
-				#print (idx)
-				#print ("syn1 shape: " +str(syn1.shape))
-				#print (synWeightUpdate1.shape)
 				syn1 -= eta * synWeightUpdate1.T
 				syn0 -= alpha * synWeightUpdate0.T
 
 				prevSynWeightUpdate0 = synWeightUpdate0
 				prevSynWeightUpdate1 = synWeightUpdate1
-				'''
-				layer0 = X
-				layer1 = self.sigmoid(np.dot(layer0,syn0))
 
-				errorLayer1 = layer1 - y
-
-				sumError = 0.0
-				for i in range(len(errorLayer1)):
-					predictionError = errorLayer1[i]
-					sumError += (predictionError ** 2)
-				
-				error = (sumError / float(len(errorLayer1))) 
-				error = error ** 0.5
-				if idx%10 == 0 or idx == 1:
-					print("Error is: " + str(error))
-				deltaLayer1 = errorLayer1 * self.sigmoid_output_to_derivative(layer1)
-				syn0Deriv = np.dot(layer0.T,deltaLayer1)
-
-				syn0 += alpha*syn0Deriv
-				'''
 			#np.savetxt("./data/NN.csv", layer2, delimiter = ",", fmt = "%s")
 		self.syn0 = syn0
 		self.syn1 = syn1
 		return 0
 
 	def test_nn(self):
+		#Test function to implement Neural network
 		setTesting = ((self.setTesting-self.meanTraining)/self.stDevTraining)
 		#prediction = setTesting.dot(self.weights)+self.bias
 
@@ -358,8 +336,9 @@ class LineGradDesc:
 			csvOutput[idx+1,0] = "id_" + str(idx)
 			csvOutput[idx+1,1] = float(layer2[idx,0])
 
-		np.savetxt("./data/w_pm25.csv", self.weights, delimiter = ",", fmt = "%s")
-		np.savetxt("./data/b_pm25.csv", self.bias, delimiter = ",", fmt = "%s")
+		np.savetxt("./data/syn0_pm25.csv", syn0, delimiter = ",", fmt = "%s")
+		np.savetxt("./data/syn1_pm25.csv", syn1, delimiter = ",", fmt = "%s")
 		np.savetxt("./data/NN_output.csv", layer2, delimiter=",", fmt = "%s")
 		return 0
-		
+	
+
