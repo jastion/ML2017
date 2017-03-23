e7f5b2c7f44b679b578f082ae6a30824c643953b
jastion <r05921086@ntu.edu.tw>
2017-03-24 01:31:35 +0800

    initial homework files

---
+++ hw2/hw2.py
@@ -0,0 +1,9 @@
+## Machine Learning 2017
+# Homework 2: 	
+# Instructor: 	Hung-yi Lee
+# Student: 		Michael Chiou
+# Student ID: 	R05921086
+# Email:		r05921086.ntu.edu.tw 
+# Github: 		https://github.com/jastion/ML2017.git
+# Python 2.7
+
